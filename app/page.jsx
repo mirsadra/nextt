a3c92618c2cf8fdac28961bbd8a7dcf511f5da56
Mirsadra Molaei <miirsadra@gmail.com>
2023-07-23 19:50:13 +0200

    fresh point to start

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+// Run `rafce` to create a new component
+// in Next.js, we no longer need to specify the react import
+// import React from 'react'
+
+const SadraHome = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SadraHome
