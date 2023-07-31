c1a8fde586a176af46b9c9e8f8c151cca66085fe
Mirsadra Molaei <miirsadra@gmail.com>
2023-08-01 00:05:54 +0200

    create new route.js for api in previous step

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee968c6c839c34b4"; }
+/******/ 		__webpack_require__.h = function() { return "fa55d77e70b8a04c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
