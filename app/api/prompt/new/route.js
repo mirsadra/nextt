6a8f732b4007c0680824d1450fe0dd6ccb52b3a1
Mirsadra Molaei <miirsadra@gmail.com>
2023-08-01 00:24:43 +0200

    connect to db, create model for prompt by new file

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,4 +1,11 @@
+import { connectToDB } from "@utils/database";  // we'll need this to connect to the database
+
+
 export const POST = async (req) => {
     // first we want to grab things we've passed through the post request
     const { userId, prompt, tag } = await req.json(); 
+
+    try {
+        await connectToDB();  // connect to the database and then create a new prompt
+    } catch (error) {
 }
