6793fd91702b90cf69dbc0981bee7e59cf3761fe
Mirsadra Molaei <miirsadra@gmail.com>
2023-08-01 21:07:18 +0200

    create prompt done. next is Feed.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
-      <h1 className="head_text text-left"> <span className="blue_gradient">{type} Post</span> </h1>
+      <h1 className="head_text text-left"> <span className="blue_gradient">{type} Post </span> </h1>
       <p className="desc text-left max-w-md"> {type} and share NFT prompt with ChatGPT, and let your idea run wild with the power of blockchain.</p>
       <form 
         onSubmit={handleSubmit}
